807f77f28dd9d43f472bc6b0cc5de2da07dead2f
Sangheon Lee <wwdbsh@gmail.com>
2020-09-20 19:21:37 -0400

    2nd update

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from "../Styles/GlobalStyles";
+import Theme from "../Styles/Theme";
 
-function App() {
-  return (
-    <div className="App"/>
-  );
-}
-
-export default App;
+export default () => (
+  <ThemeProvider theme={Theme}>
+    <GlobalStyles/>
+  </ThemeProvider>
+);
