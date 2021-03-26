d949ffb48b0645dd1390623bb8b06cc0ecf07aa3
Sangheon Lee <wwdbsh@gmail.com>
2021-03-26 17:46:16 -0400

    20210326:update

--- src/Apollo/Client.js
+++ src/Apollo/Client.js
@@ -2,7 +2,7 @@ import ApolloClient from "apollo-boost";
 import { defaults, resolvers } from "./LocalState";
 
 export default new ApolloClient({
-    uri:"http://localhost:4000",
+    uri:"http://ec2-3-87-247-227.compute-1.amazonaws.com:4000",
     clientState:{
         defaults,
         resolvers
